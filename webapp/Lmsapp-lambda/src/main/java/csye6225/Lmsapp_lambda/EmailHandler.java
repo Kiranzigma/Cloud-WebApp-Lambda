4d3fcee6034ca7147243331bd8b346cce6848c53
Kiranzigma <kathiresan.k@husky.neu.edu>
2019-08-10 20:28:09 -0400

    HTTPS

--- webapp/Lmsapp-lambda/src/main/java/csye6225/Lmsapp_lambda/EmailHandler.java
+++ webapp/Lmsapp-lambda/src/main/java/csye6225/Lmsapp_lambda/EmailHandler.java
@@ -58,10 +58,10 @@ public class EmailHandler implements RequestHandler<SNSEvent, Object>
 
             try {
                 String TO = request.getRecords().get(0).getSNS().getMessage();
-                String token = request.getRecords().get(0).getSNS().getMessageId();
+                String token = context.getAwsRequestId();
                 AmazonSimpleEmailService client = AmazonSimpleEmailServiceClientBuilder.defaultClient();
                 SendEmailRequest req = new SendEmailRequest().withDestination(new Destination().withToAddresses(TO))
-                		.withMessage(new Message().withBody(new Body().withHtml(new Content().withCharset("UTF-8").withData("Please click on the below link to reset the password<br/>"+ "<p><a href='#'>http://csye6225-su19-"+System.getenv("AWS_DOMAIN_NAME")+".me/reset?email="+TO+"&token="+token+"</a></p>")))
+                		.withMessage(new Message().withBody(new Body().withHtml(new Content().withCharset("UTF-8").withData("Please click on the below link to reset the password<br/>"+ "<p><a href='#'>https://csye6225-su19-"+System.getenv("AWS_DOMAIN_NAME")+".me/reset?email="+TO+"&token="+token+"</a></p>")))
                                         .withSubject(
                                                 new Content().withCharset("UTF-8")
                                                         .withData("Password Reset Link For LMSApp")))
